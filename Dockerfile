90a6d5b4d456b0e6b202fb67be1fe224bf78bf48
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-12-13 09:42:53 +0400

    trying with conda-build-all instead

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,5 @@
 from continuumio/miniconda3
 
-COPY /home/jillian/.eb/software/Anaconda3/5.0.1/pkgs /anaconda-pkg-cache
-COPY /home/jillian/.eb/software/Anaconda3/5.0.1/conda-bld /anaconda-conda-bld
-
-ENV CONDA_PKGS_DIRS /anaconda/pkg-cache 
-ENV CONDA_BLD_PATH /anaconda-conda-bld 
-
-RUN conda add --channels file://anaconda-conda-bld
-RUN conda index /anaconda-conda-bld/linux-64 
-
-RUN cat ~/.condarc
-
-RUN conda install -y gencore_test
+ARG CONDA_PKG=local
+ENV CONDA_PKG ${CONDA_PKG}
+RUN echo ${CONDA_PKG}
---
+++ scripts/travis-run-conda-build-all.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -euo pipefail
+# Set some defaults
+set +u
+[[ -z $DOCKER_ARG ]] && DOCKER_ARG=""
+[[ -z $TRAVIS ]] && TRAVIS="false"
+[[ -z $BIOCONDA_UTILS_LINT_ARGS ]] && BIOCONDA_UTILS_LINT_ARGS=""
+[[ -z $RANGE_ARG ]] && RANGE_ARG="--git-range master HEAD"
+[[ -z $DISABLE_BIOCONDA_UTILS_BUILD_GIT_RANGE_CHECK  ]] && DISABLE_BIOCONDA_UTILS_BUILD_GIT_RANGE_CHECK="false"
+[[ -z $SKIP_LINTING ]] && SKIP_LINTING=false
+set -u
+
+if [[ $TRAVIS_BRANCH != "master" && $TRAVIS_BRANCH != "bulk" && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_REPO_SLUG == "$MY_TRAVIS_REPO_SLUG" ]]
+then
+    echo ""
+    echo "Tests are skipped for pushes to the main bioconda-recipes repo."
+    echo "If you have opened a pull request, please see the full tests for that PR."
+    echo "See https://bioconda.github.io/build-system.html for details"
+    echo ""
+    exit 0
+fi
+
+export PATH=/anaconda/bin:$PATH
+
+# TODO Add another upload
+if [[ ( $TRAVIS_BRANCH == "master" || $TRAVIS_BRANCH == "bulk" ) && "$TRAVIS_PULL_REQUEST" == "false" && $TRAVIS_REPO_SLUG == "$MY_TRAVIS_REPO_SLUG" ]]
+then
+        UPLOAD_ARG="--upload-channels nyuad-cgsb "
+fi
+
+
+if [[ $DISABLE_BIOCONDA_UTILS_BUILD_GIT_RANGE_CHECK == "true" ]]
+then
+    echo
+    echo "DISABLE_BIOCONDA_UTILS_BUILD_GIT_RANGE_CHECK is true."
+    echo "A comprehensive check will be performed to see what needs to be built."
+    RANGE_ARG=""
+fi
+
+set -x
+conda-build-all recipes --inspect-channels nyuad-cgsb $UPLOAD_ARG
+ls -lahR $CONDA_BLD_PATH
+
+docker images
+set +x
