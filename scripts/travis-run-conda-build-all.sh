57552ee950f1eb3e430b4667435e2fff7e7b1b0c
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-12-13 14:08:48 +0400

    WHERE IST HE TRAVIS WAIT

--- scripts/travis-run-conda-build-all.sh
+++ scripts/travis-run-conda-build-all.sh
@@ -41,14 +41,14 @@ set -x
 
 export PATH=/home/travis/anaconda3/bin:$PATH
 conda install -y conda-build-all
-travis_wait 30 conda-build-all recipes --inspect-channels nyuad-cgsb $UPLOAD_ARG
+conda-build-all recipes --inspect-channels nyuad-cgsb $UPLOAD_ARG
 ls -lahR $CONDA_BLD_PATH
 
 echo "We should be building things now...."
 
 cp Dockerfile /opt/anaconda3
 cd /opt/anaconda3
-travis_wait 30 docker build --build-arg CONDA_PKG='gencore_rnaseq-1.0-r3.4.1_1.tar.bz2'
+docker build --build-arg CONDA_PKG='gencore_rnaseq-1.0-r3.4.1_1.tar.bz2'
 
 docker images
 set +x
